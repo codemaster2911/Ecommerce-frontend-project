6957f775050bdb92daebfc66c0adb6ccdb7d32a9
codemaster2911 <2911.tushargoyal@gmail.com>
2022-03-11 18:35:51 +0530

    second commit

---
+++ src/component/Admin/Dashboard.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect } from "react";
+import "./Sidebar";
+import "./Dashboard.css";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+import { Doughnut, Line } from "react-chartjs-2";
+import { useSelector, useDispatch } from "react-redux";
+import { getAdminProduct } from "../../actions/productAction";
+import Sidebar from "./Sidebar";
+import MetaData from "../layout/MetaData";
+import { getAllOrders } from "../../actions/orderAction";
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  const { products } = useSelector((state) => state.products);
+  const { orders } = useSelector((state) => state.allOrders);
+
+  let outOfStock = 0;
+  let length = 0;
+
+  products &&
+    products.forEach((item) => {
+      if (item.stock === 0) {
+        outOfStock += 1;
+      }
+    });
+
+  products &&
+    products.forEach((item) => {
+      length += 1;
+    });
+
+  useEffect(() => {
+    dispatch(getAdminProduct());
+    dispatch(getAllOrders());
+  }, [dispatch]);
+
+  const lineState = {
+    labels: ["Initial Amount", "Amount Earned"],
+    datasets: [
+      {
+        label: "TOTAL AMOUNT",
+        backgroundColor: ["tomato"],
+        hoverBackgroundColor: ["rgb(197, 72, 49)"],
+        data: [0, 4000],
+      },
+    ],
+  };
+
+  const doughnutState = {
+    labels: ["Out of Stock", "InStock"],
+    datasets: [
+      {
+        backgroundColor: ["#00A6B4", "#6800B4"],
+        hoverBackgroundColor: ["#4B5000", "#35014F"],
+        data: [outOfStock, length - outOfStock],
+      },
+    ],
+  };
+
+  return (
+    <div className="dashboard">
+      <MetaData title="Dashboard - Admin Panel" />
+      <Sidebar />
+
+      <div className="dashboardContainer">
+        <Typography component="h1">Dashboard</Typography>
+
+        <div className="dashboardSummary">
+          <div>
+            <p>
+              Total Amount <br /> ₹{1}
+            </p>
+          </div>
+          <div className="dashboardSummaryBox2">
+            <Link to="/admin/products">
+              <p>Product</p>
+              <p>{products && products.length}</p>
+            </Link>
+            <Link to="/admin/orders">
+              <p>Orders</p>
+              {/* <p>50</p> */}
+              <p>{orders && orders.length}</p>
+            </Link>
+            <Link to="/admin/users">
+              <p>Users</p>
+              <p>100</p>
+              {/* <p>{users && users.length}</p> */}
+            </Link>
+          </div>
+        </div>
+
+        <div className="lineChart">
+          <Line data={lineState} />
+        </div>
+
+        <div className="doughnutChart">
+          <Doughnut data={doughnutState} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./Store";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+(async () => {
+  const stripePromise = loadStripe(
+    "pk_test_51KZZQ0SDmP6B2opgiZ1Tk09ObFm7l9WU5o3FZl9BxTr57Z7CBC6oIhy7WBAUnTczvQosTc2mZFqJRmDLSSO5FlQV00n7fe9yv2"
+  );
+  ReactDOM.render(
+    <Provider store={store}>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
+      </AlertProvider>
+    </Provider>,
+    document.getElementById("root")
+  );
+})();
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <AlertProvider template={AlertTemplate} {...options}>
+//       <App />
+//     </AlertProvider>
+//   </Provider>,
+//   document.getElementById("root")
+// );
